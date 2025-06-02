5a0e6fc5ae61b8fd18d21dd39ad5d4ef58d50a30
nonyx777 <nahomretebo@gmail.com>
2025-06-02 13:38:19 +0300

    Generated Array Mesh instead of a prebuilt mesh, removed height property

--- Resources/planet_property.gd
+++ Resources/planet_property.gd
@@ -4,16 +4,11 @@ class_name PlanetProperty extends Resource
 	set(p_radius):
 		radius = p_radius
 		changed.emit()
-@export var height: float:
-	set(p_height):
-		height = p_height
-		changed.emit()
 @export var color: Color:
 	set(p_color):
 		color = p_color
 		changed.emit()
 
-func _init(p_radius = 0.5, p_height = 1.0, p_color = Color.ANTIQUE_WHITE) -> void:
+func _init(p_radius = 0.5, p_color = Color.ANTIQUE_WHITE) -> void:
 	radius = p_radius
-	height = p_height
 	color = p_color
--- Script/procedural_planet.gd
+++ Script/procedural_planet.gd
@@ -2,6 +2,10 @@
 extends Node3D
 
 var material: Material
+var meshInstance: MeshInstance3D
+var primitive_mesh: SphereMesh
+var array_mesh: ArrayMesh
+var mdt: MeshDataTool
 
 @export var property: PlanetProperty:
 	set(p_resource):
@@ -16,22 +20,70 @@ var material: Material
 		planetNoise = p_planetNoise
 		planetNoise.changed.connect(_on_noise_changed)
 
-@export var planet: MeshInstance3D
+@export_tool_button("Vertex") var change_vertex_tool_button = change_vertex
 
 func _on_property_changed():
-	if material != null:
-		planet.mesh.radius = property.radius
-		planet.mesh.height = property.height
-		material.albedo_color = property.color;
-	material = planet.mesh.surface_get_material(0)
+	# Remove all MeshInstance3D children
+	#clear_mesh()
+	
+	if material == null:
+		material = Material.new()
+	if primitive_mesh == null:
+		primitive_mesh = SphereMesh.new()
+	if array_mesh == null:
+		array_mesh = ArrayMesh.new()
+	if mdt == null:
+		mdt = MeshDataTool.new()
+		
+	array_mesh.clear_surfaces()
+	material.albedo_color = property.color;
+	var arrays = primitive_mesh.surface_get_arrays(0)
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	mdt.create_from_surface(array_mesh, 0)
+	for i in range(mdt.get_vertex_count()):
+		var vertex = mdt.get_vertex(i)
+		vertex += mdt.get_vertex_normal(i) * property.radius
+		mdt.set_vertex(i, vertex)
+	mdt.commit_to_surface(array_mesh)
+	if meshInstance == null:
+		meshInstance = MeshInstance3D.new()
+		add_child(meshInstance)
+	meshInstance.mesh = array_mesh
+	meshInstance.mesh.surface_set_material(1, material)
 
 func _on_noise_changed():
-	if material != null:
-		material.set_shader_parameter("strength", planetNoise.strength)
-		material.set_shader_parameter("octaves", planetNoise.octaves)
-		material.set_shader_parameter("baseRoughness", planetNoise.baseRoughness)
-		material.set_shader_parameter("roughness", planetNoise.roughness)
-		material.set_shader_parameter("persistance", planetNoise.persistance)
-		material.set_shader_parameter("center", planetNoise.center)
-		material.set_shader_parameter("minValue", planetNoise.minValue)
-	material = planet.mesh.surface_get_material(0)
+	if material == null:
+		material = Material.new()
+	material.set_shader_parameter("strength", planetNoise.strength)
+	material.set_shader_parameter("octaves", planetNoise.octaves)
+	material.set_shader_parameter("baseRoughness", planetNoise.baseRoughness)
+	material.set_shader_parameter("roughness", planetNoise.roughness)
+	material.set_shader_parameter("persistance", planetNoise.persistance)
+	material.set_shader_parameter("center", planetNoise.center)
+	material.set_shader_parameter("minValue", planetNoise.minValue)
+
+func change_vertex():
+	if material == null:
+		return
+	var primitive_mesh = SphereMesh.new()
+	var array_mesh = ArrayMesh.new()
+	var arrays = primitive_mesh.surface_get_arrays(0)
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	var mdt = MeshDataTool.new()
+	mdt.create_from_surface(array_mesh, 0)
+	for i in range(mdt.get_vertex_count()):
+		var vertex = mdt.get_vertex(i)
+		vertex += mdt.get_vertex_normal(i)
+		mdt.set_vertex(i, vertex)
+	var _radius:float = (mdt.get_vertex(0) - Vector3.ZERO).length()
+	property.radius = 1.0
+	mdt.commit_to_surface(array_mesh)
+	material.albedo_color = property.color
+	#planet.mesh = array_mesh
+	#planet.mesh.surface_set_material(0, material)
+
+func clear_mesh():
+	for child in get_children():
+		if child is MeshInstance3D:
+			remove_child(child)
+			child.queue_free()
