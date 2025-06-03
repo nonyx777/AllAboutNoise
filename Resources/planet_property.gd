829b581d5e26c60b464f8b1ce8d0e4cf4f201a34
nonyx777 <nahomretebo@gmail.com>
2025-06-03 14:23:19 +0300

    High resolution Sphere, input parameter for radial segments and rings

--- Resources/planet_property.gd
+++ Resources/planet_property.gd
@@ -4,11 +4,21 @@ class_name PlanetProperty extends Resource
 	set(p_radius):
 		radius = p_radius
 		changed.emit()
+@export var radial_segments: int:
+	set(p_radial_segments):
+		radial_segments = p_radial_segments
+		changed.emit()
+@export var rings: int:
+	set(p_rings):
+		rings = p_rings
+		changed.emit()
 @export var color: Color:
 	set(p_color):
 		color = p_color
 		changed.emit()
 
-func _init(p_radius = 0.5, p_color = Color.ANTIQUE_WHITE) -> void:
+func _init(p_radius = 0.5, p_radial_segments = 256, p_rings = 224, p_color = Color.ANTIQUE_WHITE) -> void:
 	radius = p_radius
+	radial_segments = p_radial_segments
+	rings = p_rings
 	color = p_color
